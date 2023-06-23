d5fca63e2febfb4b19c15341e19674454217df4b
rmendezn <47116725+rmendezn@users.noreply.github.com>
2023-06-23 07:48:15 -0600

    Update Covid Portfolio Project scripts v1.sql

--- Covid Portfolio Project scripts v1.sql
+++ Covid Portfolio Project scripts v1.sql
@@ -1,3 +1,7 @@
+/*
+Covid 19 Data Exploration
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types.
+
 SELECT *
 FROM CovidDeaths
 WHERE continent is not null
